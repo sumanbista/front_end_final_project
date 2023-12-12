27c6b4b3870609fa12a875fa32f53a77eaee8523
sumanbista <bistasuman17@gmail.com>
2023-12-12 00:31:35 -0500

    sponsor

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+
+//  sponsor logo 
+const sponsorLogoContainer = document.createElement('div');
+sponsorLogoContainer.id = 'sponsor-logo';
+sponsorLogoContainer.classList.add('sponsor-logo');
+
+const sponsorHeading = document.createElement('h2');
+sponsorHeading.textContent = 'Our Sponsors';
+
+sponsorLogoContainer.appendChild(sponsorHeading);
+
+const sponsors = [
+  {
+    name: 'MRF Tyres',
+    logoUrl: 'https://resources.pulse.icc-cricket.com/ICC/document/2023/07/12/ec719b27-9cd2-4636-9085-646f0e07b2cf/mrf-tyres-1.svg'
+  },
+  {
+    name: 'Booking',
+    logoUrl: 'https://resources.pulse.icc-cricket.com/ICC/document/2023/07/12/b0aa6dfb-931e-414d-b4f3-cd0c76e1fca2/booking.com_Light.svg'
+  },
+  {
+    name:'Indusind Bank',
+    logoUrl:'https://resources.pulse.icc-cricket.com/ICC/document/2023/09/27/5b8d4047-922d-44d2-bc67-f13e07a49a73/Induslnd_Bank.svg'  
+  },
+  {
+    name:'Mastercard',
+    logoUrl:'https://resources.pulse.icc-cricket.com/ICC/document/2023/08/23/f4c5fd86-2751-470f-b596-7925b1a0badb/Mastercard_light.svg'
+  },
+{
+    name:'Aramco',
+    logoUrl:'https://resources.pulse.icc-cricket.com/ICC/document/2023/07/12/e3a4013c-528f-453a-aed4-c173903ab85a/aramco.svg'
+},
+{
+    name:'Emirates',    
+    logoUrl:'https://resources.pulse.icc-cricket.com/ICC/document/2023/07/12/3eb26b23-9379-486d-b7f1-bac9964227e3/Emirates.svg'
+}
+];
+
+sponsors.forEach(sponsor => {
+  const sponsorItem = document.createElement('div');
+  sponsorItem.classList.add('sponsor-item');
+
+  const sponsorImage = document.createElement('img');
+  sponsorImage.classList.add('sponsor-image');
+  sponsorImage.src = sponsor.logoUrl;
+  sponsorImage.alt = sponsor.name;
+
+  sponsorItem.appendChild(sponsorImage);
+  sponsorLogoContainer.appendChild(sponsorItem);
+});
+
+document.body.appendChild(sponsorLogoContainer);
 let iccRankings = document.getElementById('icc-rankings');
 iccRankings.innerHTML = '<h1>ICC Rankings</h2>';
 
@@ -103,54 +155,54 @@ document.getElementById('batsmen-ranking-container').style.display = 'none';
 document.getElementById('bowler-ranking-container').style.display = 'none';
 
 
-//  sponsor logo 
-const sponsorLogoContainer = document.createElement('div');
-sponsorLogoContainer.id = 'sponsor-logo';
-sponsorLogoContainer.classList.add('sponsor-logo');
+// //  sponsor logo 
+// const sponsorLogoContainer = document.createElement('div');
+// sponsorLogoContainer.id = 'sponsor-logo';
+// sponsorLogoContainer.classList.add('sponsor-logo');
 
-const sponsorHeading = document.createElement('h2');
-sponsorHeading.textContent = 'Our Sponsors';
+// const sponsorHeading = document.createElement('h2');
+// sponsorHeading.textContent = 'Our Sponsors';
 
-sponsorLogoContainer.appendChild(sponsorHeading);
+// sponsorLogoContainer.appendChild(sponsorHeading);
 
-const sponsors = [
-  {
-    name: 'MRF Tyres',
-    logoUrl: 'https://resources.pulse.icc-cricket.com/ICC/document/2023/07/12/ec719b27-9cd2-4636-9085-646f0e07b2cf/mrf-tyres-1.svg'
-  },
-  {
-    name: 'Booking',
-    logoUrl: 'https://resources.pulse.icc-cricket.com/ICC/document/2023/07/12/b0aa6dfb-931e-414d-b4f3-cd0c76e1fca2/booking.com_Light.svg'
-  },
-  {
-    name:'Indusind Bank',
-    logoUrl:'https://resources.pulse.icc-cricket.com/ICC/document/2023/09/27/5b8d4047-922d-44d2-bc67-f13e07a49a73/Induslnd_Bank.svg'  
-  },
-  {
-    name:'Mastercard',
-    logoUrl:'https://resources.pulse.icc-cricket.com/ICC/document/2023/08/23/f4c5fd86-2751-470f-b596-7925b1a0badb/Mastercard_light.svg'
-  },
-{
-    name:'Aramco',
-    logoUrl:'https://resources.pulse.icc-cricket.com/ICC/document/2023/07/12/e3a4013c-528f-453a-aed4-c173903ab85a/aramco.svg'
-},
-{
-    name:'Emirates',    
-    logoUrl:'https://resources.pulse.icc-cricket.com/ICC/document/2023/07/12/3eb26b23-9379-486d-b7f1-bac9964227e3/Emirates.svg'
-}
-];
+// const sponsors = [
+//   {
+//     name: 'MRF Tyres',
+//     logoUrl: 'https://resources.pulse.icc-cricket.com/ICC/document/2023/07/12/ec719b27-9cd2-4636-9085-646f0e07b2cf/mrf-tyres-1.svg'
+//   },
+//   {
+//     name: 'Booking',
+//     logoUrl: 'https://resources.pulse.icc-cricket.com/ICC/document/2023/07/12/b0aa6dfb-931e-414d-b4f3-cd0c76e1fca2/booking.com_Light.svg'
+//   },
+//   {
+//     name:'Indusind Bank',
+//     logoUrl:'https://resources.pulse.icc-cricket.com/ICC/document/2023/09/27/5b8d4047-922d-44d2-bc67-f13e07a49a73/Induslnd_Bank.svg'  
+//   },
+//   {
+//     name:'Mastercard',
+//     logoUrl:'https://resources.pulse.icc-cricket.com/ICC/document/2023/08/23/f4c5fd86-2751-470f-b596-7925b1a0badb/Mastercard_light.svg'
+//   },
+// {
+//     name:'Aramco',
+//     logoUrl:'https://resources.pulse.icc-cricket.com/ICC/document/2023/07/12/e3a4013c-528f-453a-aed4-c173903ab85a/aramco.svg'
+// },
+// {
+//     name:'Emirates',    
+//     logoUrl:'https://resources.pulse.icc-cricket.com/ICC/document/2023/07/12/3eb26b23-9379-486d-b7f1-bac9964227e3/Emirates.svg'
+// }
+// ];
 
-sponsors.forEach(sponsor => {
-  const sponsorItem = document.createElement('div');
-  sponsorItem.classList.add('sponsor-item');
+// sponsors.forEach(sponsor => {
+//   const sponsorItem = document.createElement('div');
+//   sponsorItem.classList.add('sponsor-item');
 
-  const sponsorImage = document.createElement('img');
-  sponsorImage.classList.add('sponsor-image');
-  sponsorImage.src = sponsor.logoUrl;
-  sponsorImage.alt = sponsor.name;
+//   const sponsorImage = document.createElement('img');
+//   sponsorImage.classList.add('sponsor-image');
+//   sponsorImage.src = sponsor.logoUrl;
+//   sponsorImage.alt = sponsor.name;
 
-  sponsorItem.appendChild(sponsorImage);
-  sponsorLogoContainer.appendChild(sponsorItem);
-});
+//   sponsorItem.appendChild(sponsorImage);
+//   sponsorLogoContainer.appendChild(sponsorItem);
+// });
 
-document.body.appendChild(sponsorLogoContainer);
+// document.body.appendChild(sponsorLogoContainer);
